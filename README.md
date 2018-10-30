c804011f4e44d466d035ccde12289df463b41f40
Vaishak Kallore <vaikoovery@gmail.com>
2018-10-30 16:32:17 +0530

    Lazy load with Intersection Observer API

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # browser-apis
+
+Referring to the slides of [Swapnil Agarwal](https://gitpitch.com/swapagarwal/new-kids-in-browserland), [Source Code of slides](https://github.com/swapagarwal/new-kids-in-browserland/) thought to give a try and take demo for my team.
+
+The code sample for following can be found.
+
+1. [Intersection Observer API](./intersection-observer-api.html)
+2. Credential Management API
+3. Network Information API
+4. Web Share API
+5. Device Memory API
+6. Payment Request API
---
+++ src/js/intersection-observer-api.js
@@ -0,0 +1,79 @@
+/**
+ * Original Source: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+ */
+
+var prevRatio = 0.0
+var increasingColor = 'rgba(40, 40, 190, ratio)'
+var decreasingColor = 'rgba(190, 40, 40, ratio)'
+
+// Set things up.
+
+window.addEventListener(
+  'load',
+  function(event) {
+    observerElements = document.querySelectorAll('.observer img')
+    observerElements.forEach(function(observerElement) {
+      createObserver(observerElement)
+    })
+  },
+  false
+)
+function createObserver(observerElement) {
+  var options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: buildThresholdList()
+  }
+
+  var observer = new IntersectionObserver(handleIntersect, options)
+  observer.observe(observerElement)
+}
+function buildThresholdList() {
+  var thresholds = []
+  var numSteps = 20
+
+  for (var i = 1.0; i <= numSteps; i++) {
+    var ratio = i / numSteps
+    thresholds.push(ratio)
+  }
+
+  thresholds.push(0)
+  return thresholds
+}
+function handleIntersect(entries, observer) {
+  entries.forEach(function(entry) {
+    if (entry.intersectionRatio > prevRatio) {
+      entry.target.style.backgroundColor = increasingColor.replace(
+        'ratio',
+        entry.intersectionRatio
+      )
+    } else {
+      entry.target.style.backgroundColor = decreasingColor.replace(
+        'ratio',
+        entry.intersectionRatio
+      )
+    }
+
+    var toFixed = (entry.intersectionRatio * 100).toFixed(2)
+    var images = []
+    if (entry.target.nodeName.toUpperCase() !== 'IMG') {
+      images = entry.target.querySelectorAll('img')
+    } else {
+      images.push(entry.target)
+    }
+    if (toFixed > 0) {
+      images.forEach(function(img) {
+        var dataSrc = img.getAttribute('data-src')
+        if (img.src !== dataSrc) {
+          img.src = dataSrc
+        }
+      })
+    }
+    var observerIntRatio = entry.target.querySelector('#observerIntRatio')
+    if (typeof observerIntRatio !== undefined && observerIntRatio !== null) {
+      observerIntRatio.innerHTML = toFixed + '%'
+    }
+
+    prevRatio = entry.intersectionRatio
+  })
+}
