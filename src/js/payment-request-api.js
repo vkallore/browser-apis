2b4637c3d9fdfa4f7eaa00132601b6f17f15e24c
Vaishak Kallore <vaikoovery@gmail.com>
2018-11-15 15:46:15 +0530

    WIP - Payment request API

--- src/js/payment-request-api.js
+++ src/js/payment-request-api.js
@@ -1,20 +1,43 @@
 if (location.protocol === 'https:') {
-  const request = new PaymentRequest(
-    [
+  const supportedInstruments = [
+    {
+      supportedMethods: 'basic-card'
+    }
+  ]
+
+  const details = {
+    total: { label: 'Donation', amount: { currency: 'USD', value: '65.00' } },
+    displayItems: [
       {
-        supportedMethods: 'https://bobpay.xyz/pay',
-        data: {
-          supportedNetworks: ['visa', 'master', 'jcb'],
-          supportedTypes: ['credit', 'debit', 'prepaid']
-        }
+        label: 'Original donation amount',
+        amount: { currency: 'USD', value: '65.00' }
       }
     ],
-    {
-      total: {
-        label: 'total',
-        amount: { value: '10', currency: 'USD' }
+    shippingOptions: [
+      {
+        id: 'standard',
+        label: 'Standard shipping',
+        amount: { currency: 'USD', value: '0.00' },
+        selected: true
       }
-    }
-  )
-  console.log(request)
+    ]
+  }
+
+  const options = { requestShipping: true }
+  async function doPaymentRequest() {
+    const request = new PaymentRequest(supportedInstruments, details, options)
+    // Add event listeners here.
+    // Call show() to trigger the browser's payment flow.
+    const response = await request.show()
+    console.log(response)
+    // Process payment.
+    const json = response.toJSON()
+    const httpResponse = await fetch('/pay/', { method: 'POST', body: json })
+    const result = httpResponse.ok ? 'success' : 'failure'
+
+    await response.complete(result)
+  }
+  doPaymentRequest()
+} else {
+  alert('Payment Request API works only on HTTPS urls')
 }
